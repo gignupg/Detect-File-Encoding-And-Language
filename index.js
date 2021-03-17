b0c73746bb3f9b16c70127925aefd15b2bbc8ba4
gignupg <linus.komnick@googlemail.com>
2021-03-17 15:45:50 +0100

    Working again

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ module.exports = (file) => {
     return new Promise((resolve, reject) => {
         const fileInfo = {};
         const language = require('./languageObject.js');
+        let utf8 = true;
+
+        // Making sure to reset the count
         language.forEach(elem => elem.count = 0);
 
         const utfReader = new FileReader();
 
         utfReader.onload = () => {
-            const content = utfReader.result;
+            const utfContent = utfReader.result;
 
-            let utf8 = true;
-
-            for (let b = 0; b < content.length; b++) {
+            for (let b = 0; b < utfContent.length; b++) {
                 // If � is encountered it's definitely not utf8!
-                if (content[b] === "�") {
+                if (utfContent[b] === "�") {
                     utf8 = false;
                     break;
                 }
@@ -21,35 +22,19 @@ module.exports = (file) => {
 
             if (utf8) {
                 // Counting how many matches we can find for each language
-                language.forEach(lang => {
-                    const matches = content.match(lang.utfRegex);
+                findMatches(utfContent, "utfRegex");
 
-                    if (matches) {
-                        lang.count = matches.length;
-                    }
-                });
-
-                console.log(language);
-
-                fileInfo.language = language.reduce((acc, val) => acc.count > val.count ? acc : val).name;
-
-                console.log(fileInfo.language);
-
-
-                // fileInfo.confidence = calculateConfidence();
-                // fileInfo.encoding = "UTF-8";
-
-                // resolve(fileInfo);
+                sendResponse();
 
             } else {
                 const isoReader = new FileReader();
 
                 isoReader.onload = () => {
-                    fileInfo.language = Object.keys(language).reduce((a, b) => language[a] > language[b] ? a : b);
-                    fileInfo.confidence = calculateConfidence();
-                    fileInfo.encoding = detectEncoding(fileInfo.language);
+                    const isoContent = isoReader.result;
+                    // Counting how many matches we can find for each language
+                    findMatches(isoContent, "isoRegex");
 
-                    resolve(fileInfo);
+                    sendResponse();
                 };
 
                 isoReader.onerror = reject;
@@ -59,6 +44,35 @@ module.exports = (file) => {
 
         utfReader.onerror = reject;
         utfReader.readAsText(file, "UTF-8");
+
+        function findMatches(reader, regex) {
+            language.forEach(lang => {
+                const matches = reader.match(lang[regex]);
+
+                if (matches) lang.count = matches.length;
+            });
+        }
+
+        function sendResponse() {
+            fileInfo.language = language.reduce((acc, val) => acc.count > val.count ? acc : val).name;
+
+            // Determine the encoding
+            if (utf8) {
+                fileInfo.encoding = "UTF-8";
+
+            } else {
+                const pos = language.findIndex(elem => elem.name === fileInfo.language);
+                fileInfo.encoding = language[pos].isoEncoding;
+            }
+
+            // fileInfo.confidence = calculateConfidence();
+
+            // Temporary console log
+            const testPos = language.findIndex(elem => elem.name === fileInfo.language);
+            console.log(fileInfo.language, language[testPos].count);
+
+            resolve(fileInfo);
+        }
 
         function calculateConfidence() {
             const secondLanguage = Object.keys(language).reduce((a, b) => {
--- languageObject.js
+++ languageObject.js
@@ -1,4 +1,4 @@
-const flag = "gi";
+﻿const flag = "gi";
 
 const sharedRegex = {
     czech: new RegExp("jsem|jsi", flag),
