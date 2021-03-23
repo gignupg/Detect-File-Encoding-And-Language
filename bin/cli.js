a30f42e676d674d37863c098ded9d9f1c3da7900
gignupg <linus.komnick@googlemail.com>
2021-03-23 14:42:45 +0100

    Trying to add my cli to the package

---
+++ bin/cli.js
@@ -0,0 +1,15 @@
+const languageEncoding = require("./index.js");
+
+const path = process.argv[2];
+
+const tooManyArguments = process.argv[3];
+
+if (tooManyArguments) console.log('Error! Too many arguments passed in. Only one argument can be passed in. If your path or file name contain spaces, try to surround the whole file path with quotes!');
+
+languageEncoding(path, true)
+    .then(fileInfo => {
+        console.log(fileInfo);
+    })
+    .catch(error => {
+        console.log(error);
+    });;
