be5d8d8492fa393751a3a356396ceb2a3e330a10
gignu <gignupg@gmail.com>
2021-04-19 23:57:08 +0200

    Updated the files array in package.json because language-config has been renamed to config

--- bin/cli.js
+++ bin/cli.js
@@ -6,12 +6,15 @@ const path = process.argv[2];
 
 const tooManyArguments = process.argv[3];
 
-if (tooManyArguments) console.log('Error! Too many arguments passed in. Only one argument can be passed in. If your path or file name contain spaces, try to surround the whole file path with quotes!');
+if (tooManyArguments)
+  console.log(
+    "Error! Too many arguments passed in. Only one argument can be passed in. If your path or file name contain spaces, try to surround the whole file path with quotes!"
+  );
 
 languageEncoding(path)
-    .then(fileInfo => {
-        console.log(fileInfo);
-    })
-    .catch(error => {
-        console.log(error);
-    });;
+  .then((fileInfo) => {
+    console.log(fileInfo);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
