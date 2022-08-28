1faec289fd2c094c2d550139d4149aace32255be
Stephen Sherratt <stephen@sherra.tt>
2022-08-28 20:28:49 +1000

    Correct argument type in typescript signature

--- src/index-node.d.ts
+++ src/index-node.d.ts
@@ -7,5 +7,5 @@ export interface FileInfo {
     },
 }
 
-declare function DetectFileEncodingAndLanguage(file: File): Promise<FileInfo>;
+declare function DetectFileEncodingAndLanguage(path: string | Buffer | URL): Promise<FileInfo>;
 export default DetectFileEncodingAndLanguage;
